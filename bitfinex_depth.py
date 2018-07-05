5d98fe8c20b23e6eaaefac9a7e35b006f650d67c
longfei <lilongfei@wallstreetcn.com>
2018-07-05 11:05:33 +0800

    update bids and asks

--- bitfinex_depth.py
+++ bitfinex_depth.py
@@ -34,7 +34,8 @@ def kafka_con():
 def ws_connect():
     global ws
     ws = create_connection("wss://api.bitfinex.com/ws/2", sslopt={"cert_reqs": ssl.CERT_NONE},
-                           http_proxy_host="localhost", http_proxy_port=1080)
+                           # http_proxy_host="localhost", http_proxy_port=1080
+                           )
     # 获取symbol
     symbol = requests.get('https://api.bitfinex.com/v1/symbols')
     symbols = symbol.text.replace('[', '').replace(']', '').replace('"', '')
@@ -70,8 +71,34 @@ while True:
                 # print(maps)
                 logging.info("maps插入一条新的映射")
         if isinstance(detail_ls, list):
+            # 如果返回的是第一个websocket大列表并且对应的socket在映射中
+            if detail_ls[0] in maps.keys() and len(detail_ls[1]) > 3:
+                pair = maps[detail_ls[0]]
+                dic = {
+                    "onlyKey": "Bitfinex_" + pair[:3] + '_' + pair[-3:],
+                    "measurement": "Depth",
+                    "timestamp": cur_time(),
+                    "tick": {
+                        'bids': [],
+                        'asks': [],
+                    }
+                }
+                for _ in detail_ls[1]:
+                    tem = {
+                        "price": _[0],
+                        "count": _[1],
+                        "amount": _[2]
+                    }
+                    if _[2] < 0:
+                        dic['tick']['bids'].append(tem)
+                    else:
+                        dic['tick']['asks'].append(tem)
+                producer.send('depth-dev', [dic])
+                logging.info("send first %s successful > timestamp--%s" % (pair, cur_time()))
+                print(("send first %s successful > timestamp--%s" % (pair, cur_time())))
+
             # 判断该id是否在映射记录里面
-            if detail_ls[0] in maps.keys() and len(detail_ls[1]) == 3:
+            elif detail_ls[0] in maps.keys() and len(detail_ls[1]) == 3:
                 pair = maps[detail_ls[0]]
                 change_type = "bid" if detail_ls[1][2] < 0 else 'ask'
                 dic = {
@@ -79,7 +106,13 @@ while True:
                     "measurement": "Depth",
                     "timestamp": cur_time(),
                     "tick": {
-                        change_type: detail_ls[1]
+                        change_type: [
+                            {
+                                'price': detail_ls[1][0],
+                                'count': detail_ls[1][1],
+                                'amount': detail_ls[1][2],
+                            }
+                        ]
                     }
                 }
                 logging.info(dic)
